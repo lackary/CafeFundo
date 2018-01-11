d4220c69b4edd9e14b88dacea3d30eab6751f2fe
lackary <lackary@gmail.com>
2018-01-11 10:26:44 +0800

    Initial Commit

---
+++ CafeFundo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CafeFundo
+//
+//  Created by lackary on 2018/1/11.
+//  Copyright © 2018年 LackLab. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
